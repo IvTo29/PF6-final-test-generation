a3e7d5bc0ef8962f070650db9f21c4692f579227
IvTo29 <tovar.ivan1997@outlook.com>
2026-03-02 12:55:33 -0500

    prueba

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import json
+import requests
+
+
+def dish_fetch(num):
+    response = requests.get(f"https://api-colombia.com/api/v1/TypicalDish/{num}")
+    informacion = response.json()
+
+    plato = {
+        "id": num,
+        "name": informacion["name"],
+        "description": informacion["description"]
+    }
+
+    return plato
+
+
 def main():
-  print("Hello learners!")
+    print("Hello learners!")
+    num = int(input("Ingrese el numero del plato: "))
+    resultado = dish_fetch(num)
+
+    print("Numero:", resultado["id"])
+    print("Nombre:", resultado["name"])
+    print("Descripcion:", resultado["description"])
+
 
 if __name__=="__main__":
-  main()
+    main()
